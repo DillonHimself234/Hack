9a0a3158dc5004b2947f8eb61fb6da9b0a166294
DillonHimself234 <90710436+DillonHimself234@users.noreply.github.com>
2022-03-28 13:21:19 -0400

    Add files via upload

---
+++ hack.py
@@ -0,0 +1,14 @@
+customer_name = input("Welcome whats your name?")
+startimg_price = 5000.25
+print(f"Hello") Kermit the frog your'e starting balace is 5000.25 $1000 Looks like you went shopping. What did you buy? a banjo How much does the banjo cost: 3000 Good day, Kermit the frog. After spending money on a banjo in the amount of $3000.0, your current checking balace is: $3000.25
+
+
+
+
+
+pay_check = $5000.25.
+print(f"Hello") How much of your paycheck would you like to deposit? 
+
+
+
+
